ad1d054935acde875222e9fce059c53721bd7a2d
Manav Kasare <manav@Manavs-MacBook-Air.local>
2021-05-31 04:33:31 +0530

    some changes

---
+++ src/controllers/Twilio.js
@@ -0,0 +1,17 @@
+import { Twilio as TwilioService } from "../services";
+import Controller from "./base";
+
+class TwilioController extends Controller {
+  sendSms = [
+    body("phoneNumber").notEmpty().withMessage("must be a valid phoneNumber."),
+    this.action(async (req, res, next) => {
+      const { phoneNumber } = req;
+      const twilioService = new TwilioService();
+      const response = await twilioService.sendSms(phoneNumber);
+      this.response(res, response.code, response.data, response.message);
+      next();
+    }),
+  ];
+}
+
+export default TwilioController;
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
-export { default as AudioEngine } from './AudioEngine';
-export { default as Cron } from './Cron';
-export { default as Drop } from './Drop';
-export { default as Analytics } from './Analytics';
-export { default as Authenticate } from './Authenticate';
+export { default as AudioEngine } from "./AudioEngine";
+export { default as Cron } from "./Cron";
+export { default as Drop } from "./Drop";
+export { default as Analytics } from "./Analytics";
+export { default as Authenticate } from "./Authenticate";
+export { default as Twilio } from "./Twilio";
