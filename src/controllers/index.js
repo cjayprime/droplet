e5dc4f21f5d1c248f7783d8b38a0c6e5a8ce7be7
Manav Kasare <manav@Manavs-MacBook-Air.local>
2021-06-05 21:01:51 +0530

    some changes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -10,67 +10,6 @@ const analytics = new Analytics();
 const authenticate = new Authenticate();
 const filter = new Filter();
 const marketing = new Marketing();
-
-export default {
-  public: [
-    // Drop
-    { method: 'GET', path: '/drops', action: drop.feed },
-    { method: 'GET', path: '/drops/user/:user_id', action: drop.feed },
-    { method: 'GET', path: '/drops/:tagORdrop_id', action: drop.single },
-    { method: 'GET', path: '/featured', action: drop.featured },
-    { method: 'GET', path: '/clouds', action: drop.getClouds },
-    { method: 'GET', path: '/subclouds', action: drop.getSubClouds },
-    { method: 'GET', path: '/download', action: drop.download },
-    { method: 'GET', path: '/waveform', action: drop.waveform },
-    { method: 'POST', path: '/validate', action: drop.validate },
-    { method: 'POST', path: '/create', action: drop.create },
-    { method: 'PUT', path: '/trim', action: drop.trim },
-
-    // Analytics
-    { method: 'POST', path: '/listen', action: analytics.recordListen },
-    { method: 'POST', path: '/like', action: analytics.recordLike },
-    {
-      method: 'POST',
-      path: '/interaction',
-      action: analytics.recordInteraction,
-    },
-    { method: 'GET', path: '/analytics', action: analytics.analyze },
-
-    // Filters
-    { method: 'POST', path: '/filter/duet', action: filter.duet },
-    {
-      method: 'POST',
-      path: '/filter/export-video',
-      action: filter.exportVideo,
-    },
-
-    // Authenticate
-    { method: 'POST', path: '/authenticate', action: authenticate.firebase },
-
-    // Sms
-    { method: 'POST', path: '/market', action: marketing.sendSms },
-
-    // Health
-    {
-      method: 'GET',
-      path: '/health',
-      action: /*authenticate.health*/ (_, res, next) => {
-        res.send('RUNNING');
-        next();
-      },
-    },
-
-    // Statuses Checker
-    {
-      method: 'GET',
-      path: '/status',
-      action: (req, res, next) => {
-        res.status(req.query.status).send('RUNNING');
-        next();
-      },
-    },
-  ]
-}
 const user = new User();
 
 export default {
@@ -110,6 +49,9 @@ export default {
     { method: 'POST', path: '/listen', action: analytics.recordListen },
     { method: 'POST', path: '/interaction', action: analytics.recordInteraction },
     { method: 'GET', path: '/analytics', action: analytics.analyze },
+
+    // Sms
+    { method: 'POST', path: '/market', action: marketing.sendSms },
 
     // Filters
     { method: 'POST', path: '/filter/duet', action: filter.duet },
